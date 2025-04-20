01bf6642a36f589bc67d2e159c9635e4dd9bbfdd
Anuradha <155190576+Anuradhaaa022@users.noreply.github.com>
2025-04-20 19:44:43 +0530

    Update userRoutes.js

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 // CORS Middleware for this router
 router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://primebidz.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
